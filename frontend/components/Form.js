e96f8c2b28ce5226ed0d438019106346bf172533
Farahim <farahim@hotmail.com>
2024-07-15 00:12:29 -0400

    complete

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import * as yup from 'yup';
 
 // 👇 Here are the validation errors you will use with Yup.
 const validationErrors = {
@@ -8,6 +9,17 @@ const validationErrors = {
 }
 
 // 👇 Here you will create your schema.
+const formSchema = yup.object().shape({
+    fullName: yup
+        .string()
+        .trim()
+        .min(3, validationErrors.fullNameTooShort)
+        .max(20, validationErrors.fullNameTooLong),
+    size: yup
+        .string()
+        .trim()
+        .oneOf(['S', 'M', 'L'], validationErrors.sizeIncorrect),
+});
 
 // 👇 This array could help you construct your checkboxes using .map in the JSX.
 const toppings = [
@@ -19,43 +31,113 @@ const toppings = [
 ]
 
 export default function Form() {
+    const [formValues, setFormValues] = useState({
+        fullName: '',
+        size: '',
+        toppings: Object.fromEntries(toppings.map(topping => [topping.topping_id, false])),
+    });
+    const [formErrors, setFormErrors] = useState({});
+    const [formValid, setFormValid] = useState(false);
+    const [formSuccess, setFormSuccess] = useState('');
+    const [formFailure, setFormFailure] = useState('');
+
+    useEffect(() => {
+        formSchema
+            .validate(formValues, { abortEarly: false })
+            .then(() => {
+                setFormValid(true);
+                setFormErrors({});
+            })
+            .catch(err => {
+                setFormValid(false);
+                const newErrors = {};
+                err.inner.forEach(error => {
+                    newErrors[error.path] = error.message;
+                });
+                setFormErrors(newErrors);
+            });
+    }, [formValues]);
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        formSchema
+            .validate(formValues)
+            .then(() => {
+                setFormSuccess(true);
+                setFormFailure(false);
+                setFormValues({
+                    fullName: '',
+                    size: '',
+                    toppings: Object.fromEntries(toppings.map(topping => [topping.topping_id, false])),
+                })
+            })
+            .catch(() => {
+                setFormFailure(true);
+                setFormSuccess(false);
+            });
+    };
+
+    const handleChange = (toppingId) => {
+        setFormValues({
+            ...formValues,
+            toppings: { ...formValues.toppings, [toppingId]: !formValues.toppings[toppingId] },
+        });
+    };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Order Your Pizza</h2>
       {true && <div className='success'>Thank you for your order!</div>}
       {true && <div className='failure'>Something went wrong</div>}
 
-      <div className="input-group">
-        <div>
-          <label htmlFor="fullName">Full Name</label><br />
-          <input placeholder="Type full name" id="fullName" type="text" />
+        <div className="input-group">
+            <div>
+                <label htmlFor="fullName">Full Name</label><br/>
+                <input
+                    placeholder="Type full name"
+                    id="fullName"
+                    type="text"
+                    value={formValues.fullName}
+                    onChange={(evt) => setFormValues({...formValues, fullName: evt.target.value})}
+                />
+            </div>
+            {formErrors.fullName && <div className='error'>{formErrors.fullName}</div>}
         </div>
-        {true && <div className='error'>Bad value</div>}
-      </div>
 
-      <div className="input-group">
-        <div>
-          <label htmlFor="size">Size</label><br />
-          <select id="size">
-            <option value="">----Choose Size----</option>
-            {/* Fill out the missing options */}
-          </select>
+        <div className="input-group">
+            <div>
+                <label htmlFor="size">Size</label><br/>
+                <select
+                    id="size"
+                    value={formValues.size}
+                    onChange={(evt) => setFormValues({...formValues, size: evt.target.value})}
+                >
+                    {/* Fill out the missing options */}
+                    <option value="">----Choose Size----</option>
+                <option value="S">Small</option>
+                <option value="M">Medium</option>
+                <option value="L">Large</option>
+            </select>
         </div>
-        {true && <div className='error'>Bad value</div>}
-      </div>
+        {formErrors.size && <div className='error'>{formErrors.size}</div>}
+    </div>
 
-      <div className="input-group">
+    <div className="input-group">
         {/* 👇 Maybe you could generate the checkboxes dynamically */}
-        <label key="1">
-          <input
-            name="Pepperoni"
-            type="checkbox"
-          />
-          Pepperoni<br />
-        </label>
-      </div>
-      {/* 👇 Make sure the submit stays disabled until the form validates! */}
-      <input type="submit" />
-    </form>
+        {toppings.map(topping => (
+            <label key={topping.topping_id}>
+                <input
+                    name={topping.topping_id}
+                    type="checkbox"
+                    checked={formValues.toppings[topping.topping_id] || false}
+                    onChange={() => handleChange(topping.topping_id)}
+                />
+                {topping.text}<br />
+            </label>
+        ))}
+    </div>
+
+    <input disabled={!formValid} type="submit" />
+</form>
   )
 }
--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import pizza from './images/pizza.jpg'
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
-  return (
+    const navigate = useNavigate()
+
+    function handleClick() {
+        navigate('/order')
+    }
+    return (
     <div>
       <h2>
         Welcome to Bloom Pizza!
